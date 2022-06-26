b6857579f6fc61837003135946c2fe2692a4fd95
Uinet <malcevdimitriy@gmail.com>
2022-06-26 19:44:48 +0300

    Initial commit

---
+++ src/main/java/ua/kpi/uinet/opencv/OpencvApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.uinet.opencv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpencvApplication.class, args);
+	}
+
+}
